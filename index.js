790f1e042f67f28ce278299603fc21f1a55b8bdc
Daniil <dan.kim.04lb@gmail.com>
2021-05-14 19:49:11 +0600

    initialize application

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const config = require('config')
+const mongoose = require('mongoose')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+    try {
+        await mongoose.connect(config.get('connect-mongodb'), {
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useNewUrlParser: true,
+        })
+        app.listen(PORT, () => {
+            console.log('The server is running on the port ' + PORT)
+        })
+    } catch (error) {
+        console.log(`Server Error ${error.message}`)
+        process.exit(1)
+    }
+}
+start()
